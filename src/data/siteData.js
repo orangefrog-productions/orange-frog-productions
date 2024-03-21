32a16cfcec973731614979f1e3c36ff84db89e0e
Trevor Rounce <trevor@switchbackcreative.ca>
2024-03-21 14:48:04 -0600

    init commit

---
+++ src/data/siteData.js
@@ -0,0 +1,9 @@
+import defaultSrc from "../../public/default.jpg";
+
+export const baseData = {
+  title: "Orange Frog Productions",
+  description:
+    "Orange Frog Productions - Bespoke Experiences for your One-Of-A-Kind Event.",
+  siteImg: `https://scoundrels-swb.netlify.app/${defaultSrc.src}`,
+  url: "https://orangefrogproductions.ca/",
+};
