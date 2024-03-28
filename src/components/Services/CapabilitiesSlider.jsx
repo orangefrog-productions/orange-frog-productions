a38c1d84d377a204531a462978ad6033fa7caef0
Trevor Rounce <trevor@switchbackcreative.ca>
2024-03-28 10:07:04 -0600

    added slider

--- src/components/Services/CapabilitiesSlider.jsx
+++ src/components/Services/CapabilitiesSlider.jsx
@@ -1,11 +1,39 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./capabilitiesSlider.scss";
+
+const settings = {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  draggable: true,
+  infinite: true,
+  speed: 500,
+  autoplay: true,
+  autoplaySpeed: 10000,
+  centerMode: false,
+  arrows: false,
+  dots: true,
+};
 
 const CapabilitiesSlider = ({ data }) => {
   return (
-    <div>
-      <h1>Slider</h1>
+    <div className="cap-slider">
+      <Slider className="cap-slider-wrap" {...settings}>
+        {data.slideImages.map((slide, index) => {
+          return (
+            <div className="cap-slider-slide" key={index}>
+              <div className="cap-slider-slide-inner">
+                <img
+                  src={slide.slideImage.sourceUrl}
+                  alt={slide.slideImage.altText}
+                />
+              </div>
+            </div>
+          );
+        })}
+      </Slider>
     </div>
   );
 };
