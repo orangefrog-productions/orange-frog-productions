61b5b44ee041ecc5668606d7b3fdfadda1dbca37
Trevor Rounce <trevor@switchbackcreative.ca>
2024-03-28 09:18:20 -0600

    added the slick slider

---
+++ src/components/Services/CapabilitiesSlider.jsx
@@ -0,0 +1,13 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const CapabilitiesSlider = ({ data }) => {
+  return (
+    <div>
+      <h1>Slider</h1>
+    </div>
+  );
+};
+
+export default CapabilitiesSlider;
