81b5dae766958eef2707261ea2c0794717b0b22f
Trevor Rounce <trevor@switchbackcreative.ca>
2024-04-02 09:13:42 -0600

    added the creative team section

---
+++ src/components/about/CreativeTeam.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import "./creativeTeam.scss";
+
+const CreativeTeam = ({ data }) => {
+  const [activeBio, setActiveBio] = useState(null);
+  return (
+    <div className="creative-team">
+      <div className="creative-team-wrapper">
+        <div className="creative-team-title">
+          <h2>Creative Team</h2>
+        </div>
+        <div className="creative-team-list">
+          {data.map((team, index) => {
+            return (
+              <div key={index} className="creative-team-list-item">
+                <div className="creative-team-list-item-image">
+                  <img
+                    src={team.creativeTeam.image.sourceUrl}
+                    alt={team.title}
+                    width="1000"
+                    height="500"
+                  />
+                </div>
+                <div className="creative-team-list-item-details">
+                  <h3>{team.title}</h3>
+                  <p>{team.creativeTeam.titlePosition}</p>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setActiveBio(index);
+                    }}
+                  >
+                    Bio
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      {!!activeBio && (
+        <div
+          className={`creative-team-modal ${activeBio !== null ? "creative-team-modal-active" : ""}`}
+        >
+          <div className="creative-team-modal-image">
+            <div className="creative-team-modal-image-wrapper">
+              <img
+                src={data[activeBio].creativeTeam.image.sourceUrl}
+                alt={data[activeBio].title}
+                width="1000"
+                height="500"
+              />
+            </div>
+          </div>
+          <div className="creative-team-modal-bio">
+            <div className="creative-team-modal-bio-titles">
+              <h3>{data[activeBio]?.title}</h3>
+              <p>{data[activeBio].creativeTeam.titlePosition}</p>
+            </div>
+            <div
+              className="creative-team-modal-bio-content"
+              dangerouslySetInnerHTML={{
+                __html: data[activeBio].creativeTeam.bio,
+              }}
+            />
+          </div>
+          <div className="creative-team-modal-button">
+            <button
+              type="button"
+              onClick={() => {
+                setActiveBio(null);
+              }}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+
+      {!!activeBio && (
+        <div
+          className="creative-team-blur"
+          onClick={() => {
+            setActiveBio(null);
+          }}
+        />
+      )}
+    </div>
+  );
+};
+
+export default CreativeTeam;
