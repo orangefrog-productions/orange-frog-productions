e4f751ece09e9ecff9f0c9ea0f1be40beac0ac15
Trevor Rounce <trevor@switchbackcreative.ca>
2024-04-02 16:54:14 -0600

    added slider nav

--- src/components/SinglePortfolio/GallerySlider.jsx
+++ src/components/SinglePortfolio/GallerySlider.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect, useRef } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -15,15 +16,48 @@ const settings = {
   centerMode: false,
   arrows: false,
   dots: true,
+  adaptiveHeight: true,
 };
 
 const GallerySlider = ({ gallery }) => {
+  const [nav1, setNav1] = useState(null);
+  const [nav2, setNav2] = useState(null);
+  let sliderRef1 = useRef(null);
+  let sliderRef2 = useRef(null);
+
+  useEffect(() => {
+    setNav1(sliderRef1);
+    setNav2(sliderRef2);
+  }, []);
+
   return (
     <div className="proj-gallery">
-      <Slider {...settings}>
+      <Slider
+        className="proj-gallery-slider-display"
+        {...settings}
+        asNavFor={nav2}
+        ref={(slider) => (sliderRef1 = slider)}
+      >
         {gallery.map((item) => {
           return (
             <div>
+              <img src={item.sourceUrl} alt={item.altText} />
+            </div>
+          );
+        })}
+      </Slider>
+
+      <Slider
+        className="proj-gallery-slider-nav"
+        slidesToShow={6}
+        swipeToSlide={true}
+        focusOnSelect={true}
+        asNavFor={nav1}
+        ref={(slider) => (sliderRef2 = slider)}
+      >
+        {gallery.map((item) => {
+          return (
+            <div className="proj-gallery-slider-nav-image">
               <img src={item.sourceUrl} alt={item.altText} />
             </div>
           );
