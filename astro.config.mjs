858aaa47d65e919c65fdd64484d4db75bc917c56
Trevor Rounce <trevor@switchbackcreative.ca>
2024-07-23 08:53:16 -0600

    site setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://orangefrogproductions.ca/",
   integrations: [react(), sitemap()],
   image: {
     domains: ["orangefrog.swbdatabases3.com"],
