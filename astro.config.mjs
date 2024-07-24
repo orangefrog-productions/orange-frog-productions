a8cd4e2366174ae43526607354e77780db7be1a4
Trevor Rounce <trevor@switchbackcreative.ca>
2024-07-24 09:11:50 -0600

    Added Google Analytics to the website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://orangefrogproductions.ca/",
-  integrations: [react(), sitemap()],
+  integrations: [
+    react(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   image: {
     domains: ["orangefrog.swbdatabases3.com"],
   },
